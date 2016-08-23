f5e657ab5aab4407319f6f6a5470d473a4ebf269
oskosk <oskosk@gmail.com>
2016-08-23 18:00:19 -0300

    Install git, nvm install 5.11, 6 and clone jetpack repo

--- provision.sh
+++ provision.sh
@@ -7,5 +7,11 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
 echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
 source /home/vagrant/.profile
 
-nvm install 0.12
-nvm alias default 0.12
+nvm install 5.11.1
+nvm install 6
+nvm alias default 5.11.1
+nvm use 5.11.1
+sudo apt-get update
+sudo apt-get -y install git
+git clone https://github.com/Automattic/jetpack
+cd jetpack
